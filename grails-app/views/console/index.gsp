43afd73d805d5a47f115e46d5a5d4442e7a43b72
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-01-17 14:04:44 -0600

    move some files over from test project

--- grails-app/views/console/index.gsp
+++ grails-app/views/console/index.gsp
@@ -1,46 +1,61 @@
 <html>
 
-	<head>
-	<title>Grails Console</title>
-	<link rel="stylesheet" href="${resource(dir: pluginPath11 + 'css', file: 'grails-console.css', plugin: 'console')}" />
-	</head>
+<head>
+  <title>Grails Console</title>
+  <link rel="stylesheet" href="${resource(dir: pluginPath11 + 'css', file: 'jquery.layout.css', plugin: 'console')}"/>
+  <link rel="stylesheet" href="${resource(dir: pluginPath11 + 'css', file: 'grails-console.css', plugin: 'console')}"/>
 
-	<body>
 
-	<div id="header">
-		<h1>Grails Debug Console</h1>
-	</div>
+  <g:javascript src='jquery-1.4.4.min.js' plugin='console'/>
+  <g:javascript src='jquery-ui-1.8.17.custom.min.js' plugin='console'/>
+  <g:javascript src='jquery.layout.js' plugin='console'/>
+  %{--<g:javascript src='jquery.hotkeys.js'               plugin='console'/>--}%
+  <g:javascript src='codemirror/js/codemirror.js' plugin='console'/>
+  <g:javascript src='grails-console/console.js' plugin='console'/>
+</head>
 
-	<div id="splitterContainer" style="height: 95%">
-		<div id='editorContainer'>
-			<div id="editor">
-				<g:textArea name="code" value="${code}" rows="25" cols="100"/>
-				<div class="buttons">
-					<div class="buttons">
-						<button id="submit" title="(CTRL + F11)">Execute</button>
-						<button id="clear" title="(Esc)">Clear Results</button>
-						<span id="progress" style="display: none;">
-							<img src="${createLinkTo(dir: pluginPath11 + 'images', file: 'spinner.gif', plugin: 'console')}" />
-							Executing Script...
-						</span>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="result"></div>
-	</div>
+<body>
 
-	<script type="text/javascript" charset="utf-8">
-	var pluginContext = "${pluginContext}";
-	var executeLink = "${executeLink}";
-	</script>
+<div id="header">
+  <h1>Grails Debug Console</h1>
 
-	<g:javascript src='jquery-1.4.4.min.js'          plugin='console'/>
-	<g:javascript src='jquery.hotkeys.js'            plugin='console'/>
-	<g:javascript src='jquery.scrollTo-min.js'       plugin='console'/>
-	<g:javascript src='splitter.js'                  plugin='console'/>
-	<g:javascript src='codemirror/js/codemirror.js'  plugin='console'/>
-	<g:javascript src='grails-console/console.js'    plugin='console'/>
+  <div class="buttons">
+    <div class="buttonset">
+      <button class="first selected horizontal button" title="Horizontal"><img
+              src="${resource(dir: pluginPath11 + 'images', file: 'v.png', plugin: 'console')}" alt="Vertical"/></button>
+      <button class="last vertical button" title="Vertical"><img src="${resource(dir: pluginPath11 + 'images', file: 'h.png', plugin: 'console')}"
+                                                                 alt="Horizontal"/></button>
+    </div>
+  </div>
+</div>
 
-	</body>
+<div id="editor">
+  <div class="buttons">
+    <button class="submit button" title="(CTRL + F11)">Execute</button>
+    <button class="clear button" title="(Esc)">Clear</button>
+  </div>
+
+  <div id="code-wrapper">
+    <g:textArea name="code" value="${code}" rows="25" cols="100"/>
+  </div>
+
+</div>
+
+<div class="east results">
+  <div class="buttons">
+    <button class="clear button" title="(Esc)">Clear</button>
+  </div>
+
+  <div id="result"></div>
+</div>
+
+<div class="south"></div>
+
+<script type="text/javascript" charset="utf-8">
+  var pluginContext = "${pluginContext}";
+  var executeLink = "${executeLink}";
+  var spinnerLink = "${createLinkTo(dir: pluginPath11 + 'images', file: 'spinner.gif', plugin: 'console')}";
+</script>
+
+</body>
 </html>
--- web-app/js/grails-console/console.js
+++ web-app/js/grails-console/console.js
@@ -1,84 +1,103 @@
 $(document).ready(function() {
 
-	var KEY_F11 = 122;
-	var KEY_ESC = 27;
+    var KEY_F11 = 122;
+    var KEY_ESC = 27;
 
-	var editor = new CodeMirror(CodeMirror.replace('code'), {
-		height: '250px', // 100% for splitter
-		content: $('#code').val(),
-		parserfile: ['../contrib/groovy/parsegroovy.js', '../contrib/groovy/tokenizegroovy.js'],
-		stylesheet: pluginContext + '/js/codemirror/contrib/groovy/groovycolors.css',
-		path: pluginContext + '/js/codemirror/js/',
-		autoMatchParens: true,
-		tabMode: 'shift',
-		textWrapping: false,
-		indentUnit: 3
-	});
+    var layout = $('body').layout({
+        north__paneSelector: '#header',
+        north__spacing_open: 0,
+        center__paneSelector: '#editor',
+        center__contentSelector: '#code-wrapper',
+        east__paneSelector: '.east',
+        east__contentSelector: '#result',
+        east__size: '50%',
+        south__paneSelector: '.south',
+        south__contentSelector: '#result',
+        south__initHidden: true,
+        south__size: '50%',
+        resizable: true,
+        fxName: ''
+    });
 
-//	$('#splitterContainer').splitter({
-//		splitHorizontal: true,
-//		A: $('#editorContainer'),
-//		B: $('#result'),
-//		closeableto: 100,
-//		animSpeed: 0
-//	});
+    var editor = new CodeMirror(CodeMirror.replace('code'), {
+        height: '100%',
+        content: $('#code').val(),
+        parserfile: ['../contrib/groovy/parsegroovy.js', '../contrib/groovy/tokenizegroovy.js'],
+        stylesheet: pluginContext + '/js/codemirror/contrib/groovy/groovycolors.css',
+        path: pluginContext + '/js/codemirror/js/',
+        autoMatchParens: true,
+        tabMode: 'shift',
+        textWrapping: false,
+        indentUnit: 3
+    });
 
-	var executeCode = function() {
-		$('#progress').show();
+    var executeCode = function () {
+        var params = {
+            code: editor.getCode(),
+            captureStdout: 'on'
+        };
+        var $result = $('<div class="script-result"><img src="'+spinnerLink+'"/> Executing Script...</div>');
+        $('#result').append($result);
 
-		var params = {
-			code: editor.getCode(),
-			captureStdout: 'on'
-		}
+        var scroll = $result.position().top + $('#result').scrollTop();
+        $('#result').animate({scrollTop: scroll});
 
-		$.post(executeLink, params, function(response) {
-			$('#progress').hide();
+        $.post(executeLink, params, function (response) {
+            var timeSpan = '<span class="result_time">' + response.totalTime + ' ms</span>'
+            if (response.exception) {
+                $result.html(timeSpan + response.exception + response.result).addClass('stacktrace').removeClass('script-result');
+            } else {
+                $result.html(timeSpan + response.output + response.result);
+            }
+        });
+    };
+//
+    $('#editor button.submit').click(executeCode);
+    $('#editor button.clear').click(function () { editor.setCode(''); });
+    $('.results button.clear').click(function () { $('#result').html(''); });
 
-			var timeSpan = '<span class="result_time">' + response.totalTime + ' ms</span>'
-			if (response.exception) {
-				$('#result').append('<div class="stacktrace">' + timeSpan +
-						'Exception: ' + response.exception + '</div>');
-			}
-			else {
-				$('#result').append('<div class="script-result">' + timeSpan +
-						response.output + response.result + '</div>');
-			}
+    $('button.vertical').click(function(event){
+        $(this).siblings('button').removeClass('selected');
+        $(this).addClass('selected');
+        $('.south').append($('.east').children());
+        layout.hide('east');
+        layout.show('south');
+        layout.initContent('south');
 
-			$('#result').scrollTo('max');
-		});
-	};
+    });
 
-	var clearResults = function() {
-		$('#result').html('');
-	};
+    $('button.horizontal').click(function(event){
+        $(this).siblings('button').removeClass('selected');
+        $(this).addClass('selected');
+        $('.east').append($('.south').children());
+        layout.hide('south');
+        layout.show('east');
+        layout.initContent('east');
+    });
+    // event handlers outside of the editor
+    //  $(document).bind('keydown', 'Ctrl+f11', executeCode);
+    //  $(document).bind('keydown', 'esc', clearResults);
 
-	$('#submit').click(executeCode);
-	$('#clear').click(clearResults);
-
-	// event handlers outside of the editor
-	$(document).bind('keydown', 'Ctrl+f11', executeCode);
-	$(document).bind('keydown', 'esc', clearResults);
-
-	setTimeout(function() {
-
-		// need a little AOP here since if we bind a function key it disables one of the
-		// letters (e.g. F11 disables z). can't bind CTRL+Enter since that's used by
-		// the editor to reformat code
-
-		var realEditorKeyDown = editor.editor['keyDown'];
-		editor.editor['keyDown'] = function() {
-			var event = arguments[0];
-			if (event.keyCode == KEY_F11 && (event.ctrlKey || event.metaKey)) {
-				executeCode();
-			}
-			return realEditorKeyDown.apply(this, arguments);
-		};
-
-		// now bind just ESC the regular way
-		editor.grabKeys(function(event) {
-			clearResults();
-		}, function(c) { return c == KEY_ESC; }
-		);
-
-	}, 1000);
+    //  setTimeout(function () {
+    //
+    //    // need a little AOP here since if we bind a function key it disables one of the
+    //    // letters (e.g. F11 disables z). can't bind CTRL+Enter since that's used by
+    //    // the editor to reformat code
+    //
+    //    var realEditorKeyDown = editor.editor['keyDown'];
+    //    editor.editor['keyDown'] = function () {
+    //      var event = arguments[0];
+    //      if (event.keyCode == KEY_F11 && (event.ctrlKey || event.metaKey)) {
+    //        executeCode();
+    //      }
+    //      return realEditorKeyDown.apply(this, arguments);
+    //    };
+    //
+    //    // now bind just ESC the regular way
+    //    editor.grabKeys(function (event) {
+    //              clearResults();
+    //            }, function (c) { return c == KEY_ESC; }
+    //    );
+    //
+    //  }, 1000);
 });
