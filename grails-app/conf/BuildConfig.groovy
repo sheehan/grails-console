41e481ed4a369c1b37883c1160e4ed440f8b880d
Burt Beckwith <beckwithb@vmware.com>
2012-02-07 18:49:37 -0500

    upgrade to 2.0

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,7 @@
+grails.project.work.dir = 'target'
+grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
+
+grails.project.dependency.resolution = {
+	inherits 'global'
+	log 'warn'
+}
