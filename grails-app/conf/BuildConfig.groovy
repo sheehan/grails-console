7cbb3c5e11ab6b1d33b23916e9a61354c1e1ff3e
Burt Beckwith <beckwithb@vmware.com>
2012-07-07 14:39:23 -0400

    update dev to grails 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,19 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
+
 	inherits 'global'
 	log 'warn'
+
+	repositories {
+		grailsCentral()
+	}
+
+	plugins {
+		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+			export = false
+		}
+	}
 }
