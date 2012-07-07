d27ac6016cb3e9fab8e33ac7f1e82cfa81d65f4b
Burt Beckwith <beckwithb@vmware.com>
2012-07-07 15:08:03 -0400

    1.2 release

--- ConsoleGrailsPlugin.groovy
+++ ConsoleGrailsPlugin.groovy
@@ -1,9 +1,14 @@
 class ConsoleGrailsPlugin {
-	String version = '1.1'
-	String grailsVersion = '1.1 > *'
+	String version = '1.2'
+	String grailsVersion = '1.3.3 > *'
 	String author = 'Siegfried Puchbauer/Mingfai Ma'
 	String authorEmail = 'siegfried.puchbauer@gmail.com / mingfai.ma@gmail.com'
-	String title = 'Web-based Groovy Console for Grails'
+	String title = 'Console Plugin'
 	String description = 'A web-based Groovy console for interactive runtime application management and debugging'
 	String documentation = 'http://grails.org/plugin/console'
+
+	String license = 'APACHE'
+	def developers = [[name: 'Burt Beckwith', email: 'burt@burtbeckwith.com']]
+	def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPCONSOLE']
+	def scm = [url: 'https://github.com/burtbeckwith/grails-console']
 }
