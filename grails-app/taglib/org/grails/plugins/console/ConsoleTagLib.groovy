cc3e8833175c6202904ecdb2e81cc8bf317717ef
Burt Beckwith <beckwithb@vmware.com>
2012-07-07 14:52:41 -0400

    use resources plugin if available

---
+++ grails-app/taglib/org/grails/plugins/console/ConsoleTagLib.groovy
@@ -0,0 +1,41 @@
+package org.grails.plugins.console
+
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder
+
+/**
+ * @author <a href='mailto:burt@burtbeckwith.com'>Burt Beckwith</a>
+ */
+class ConsoleTagLib {
+
+	static namespace = 'con'
+
+	def resources = { attrs ->
+		boolean hasResourcesPlugin = PluginManagerHolder.pluginManager.hasGrailsPlugin('resources')
+
+		if (hasResourcesPlugin) {
+			r.require(module: 'console')
+			out << r.layoutResources()
+		}
+		else {
+			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'js/CodeMirror-2.23/lib', file: 'codemirror.css', plugin: 'console')}' />"""
+			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'css', file: 'jquery.layout.css', plugin: 'console')}' />"""
+			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'css', file: 'grails-console.css', plugin: 'console')}' />"""
+		}
+	}
+
+	def layoutResources = { attrs ->
+		boolean hasResourcesPlugin = PluginManagerHolder.pluginManager.hasGrailsPlugin('resources')
+
+		if (hasResourcesPlugin) {
+			out << r.layoutResources()
+		}
+		else {
+			for (name in ['jquery-1.7.1.min', 'jquery-ui-1.8.17.custom.min',
+			              'jquery.layout', 'jquery.Storage', 'jquery.hotkeys',
+			              'CodeMirror-2.23/lib/codemirror', 'CodeMirror-2.23/mode/groovy/groovy',
+			              'grails-console/console']) {
+				out << g.javascript(src: name + '.js', plugin: 'console')
+			}
+		}
+	}
+}
--- grails-app/views/console/index.gsp
+++ grails-app/views/console/index.gsp
@@ -5,9 +5,7 @@
 <head>
   <title>Grails Console</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <link rel="stylesheet" href="${resource(dir: pluginPath11 + 'js/CodeMirror-2.23/lib', file: 'codemirror.css', plugin: 'console')}"/>
-  <link rel="stylesheet" href="${resource(dir: pluginPath11 + 'css', file: 'jquery.layout.css', plugin: 'console')}"/>
-  <link rel="stylesheet" href="${resource(dir: pluginPath11 + 'css', file: 'grails-console.css', plugin: 'console')}"/>
+  <con:resources/>
 </head>
 
 <body>
@@ -49,14 +47,7 @@
 
 <div class="south" style="display: none"></div>
 
-<g:javascript src='jquery-1.7.1.min.js' plugin='console'/>
-<g:javascript src='jquery-ui-1.8.17.custom.min.js' plugin='console'/>
-<g:javascript src='jquery.layout.js' plugin='console'/>
-<g:javascript src='jquery.Storage.js' plugin='console'/>
-<g:javascript src='jquery.hotkeys.js' plugin='console'/>
-<g:javascript src='CodeMirror-2.23/lib/codemirror.js' plugin='console'/>
-<g:javascript src='CodeMirror-2.23/mode/groovy/groovy.js' plugin='console'/>
-<g:javascript src='grails-console/console.js' plugin='console'/>
+<con:layoutResources/>
 <script type="text/javascript" charset="utf-8">
   window.gconsole = {
     pluginContext: "${resource(plugin: 'console')}",
