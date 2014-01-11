eaaef9af4ecaf93374a66c46c8ba2bde4f6506c2
Burt Beckwith <burt@burtbeckwith.com>
2014-01-11 02:11:17 -0500

    1.3 release

--- ConsoleGrailsPlugin.groovy
+++ ConsoleGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class ConsoleGrailsPlugin {
-	String version = '1.2'
+	String version = '1.3'
 	String grailsVersion = '2.0 > *'
 	String title = 'Console Plugin'
 	String description = 'A web-based Groovy console for interactive runtime application management and debugging'
