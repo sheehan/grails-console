6183271a3b50f97c845038817ae6bfc58d69ee9a
Matt Sheehan <mr.sheehan@gmail.com>
2014-09-11 20:42:20 -0500

    version.

--- ConsoleGrailsPlugin.groovy
+++ ConsoleGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class ConsoleGrailsPlugin {
-	String version = '1.4.5'
+	String version = '1.5.0'
 	String grailsVersion = '2.0 > *'
 	String title = 'Console Plugin'
 	String description = 'A web-based Groovy console for interactive runtime application management and debugging'
