3f9a5a6734b7345810de7127b874dc29f79e2063
Alexey Zhokhov <donbeave@gmail.com>
2014-10-24 00:58:28 +0800

    Upgraded to Grails 2.4.3.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,30 +1,26 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
 
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
 
 	inherits 'global'
 	log 'warn'
 
 	repositories {
-		grailsCentral()
+        grailsPlugins()
+        grailsHome()
+        mavenLocal()
+        grailsCentral()
         mavenCentral()
 	}
 
     dependencies {
-        test('org.spockframework:spock-grails-support:0.7-groovy-2.0') {
-            export = false
-        }
     }
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
 			export = false
 		}
-        test(':spock:0.7') {
-            exclude 'spock-grails-support'
-            export = false
-        }
-
 	}
 }
