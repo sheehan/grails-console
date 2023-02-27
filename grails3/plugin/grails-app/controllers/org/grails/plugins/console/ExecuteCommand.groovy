323880979c80f908cc47952d096be428fb5d53e4
Kamil Dybicz <kamil@secretescapes.com>
2023-02-27 22:31:55 +0100

    fix for an exception being thrown in case of no code execution
    grails.databinding.convertEmptyStringsToNull is true by default, so using "manual" fallback to empty string when code is false'ish. Using direct field access operator @ to accomplish that.

--- grails3/plugin/grails-app/controllers/org/grails/plugins/console/ExecuteCommand.groovy
+++ grails3/plugin/grails-app/controllers/org/grails/plugins/console/ExecuteCommand.groovy
@@ -5,4 +5,8 @@ import grails.validation.Validateable
 class ExecuteCommand implements Validateable {
 	String code
 	boolean autoImportDomains
+
+	String getCode() {
+		return this.@code ?: ''
+	}
 }
